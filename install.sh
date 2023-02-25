e93445e32eaaced214ff7861049a816ea6004638
M.Emre Nefesli <emrenefesli@outlook.com>
2023-02-25 22:01:20 +0300

    indent

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 git clone --bare git@github.com:jasyuiop/dotfilesForMyHeadless.git $HOME/.cfg
 function dotfiles {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 mkdir -p .config-backup
 dotfiles checkout
 if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
+	echo "Checked out config."
+else
+	echo "Backing up pre-existing dot files."
+	dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
